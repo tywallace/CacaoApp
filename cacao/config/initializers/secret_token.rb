e47bb2d6f6203d770b45748e882462c5d646d47f
tywallace <tylerwallace@gmail.com>
2014-11-16 13:47:21 -0500

    Created Cacao App

---
+++ cacao/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cacao::Application.config.secret_key_base = 'c8deb8cdefd5e448868b6b8ddd08d19711a337d42dcfe4eaf595770d2bd202c324608348d33a10b2b4d9839839dfc45b1ea6480b9d3b5be34deb4047fb56e838'
