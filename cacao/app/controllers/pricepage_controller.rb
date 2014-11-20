4920ac3880a981cd162aa640ca854c4e96deab98
tywallace <tylerwallace@gmail.com>
2014-11-20 17:38:29 -0500

    Working on Back end

--- cacao/app/controllers/pricepage_controller.rb
+++ cacao/app/controllers/pricepage_controller.rb
@@ -2,5 +2,9 @@ class PricepageController < ApplicationController
 	def index
 		@prices = Price.all
 	end
+
+	def show
+		@price = Price.where("region = ?", params[id])
+	end
 end
 
--- cacao/config/routes.rb
+++ cacao/config/routes.rb
@@ -1,11 +1,13 @@
 Cacao::Application.routes.draw do
   get "homepage/index"
-    get "pricepage/index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
  root 'homepage#index'
+
+ resources "pricepage"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
