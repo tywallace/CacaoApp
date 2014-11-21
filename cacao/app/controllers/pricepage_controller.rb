16f6d71963ecc4c118ef7dca06d30d2f416ac2c1
tywallace <tylerwallace@gmail.com>
2014-11-21 15:03:50 -0500

    Fixed the Backend

--- cacao/app/controllers/pricepage_controller.rb
+++ cacao/app/controllers/pricepage_controller.rb
@@ -1,10 +1,17 @@
 class PricepageController < ApplicationController
 	def index
-		@prices = Price.all
-	end
-
-	def show
-		@prices = Price.all
+		if params["region"] == "manabi"
+			@prices = Price.where(:region => "Manabi").order('date DESC').limit(10).all
+			@region = "Manabi"
+		end
+		if params["region"] == "losrios"
+			@prices = Price.where(:region => "Los Rios").order('date DESC').limit(10).all
+			@region = "Los Rios"
+		end
+		if params["region"] == "esmeraldas"
+			@prices = Price.where(:region => "Esmeraldas").order('date DESC').limit(10).all
+			@region = "Esmeraldas"
+		end
 	end
 end
 
--- cacao/db/seeds.rb
+++ cacao/db/seeds.rb
@@ -5,5 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Price.create "region" => "Manabi", "date" =>
-Date.new(2014, 11, 14), "price" => 22
+
+if Price.count != 0
+
+	Price.delete_all
+
+end
+
+Price.create "region" => "Manabi", "date" => Date.new(2014, 11, 14), "price" => 22
+Price.create "region" => "Manabi", "date" => Date.new(2014, 11, 15), "price" => 22
+Price.create "region" => "Manabi", "date" => Date.new(2014, 11, 16), "price" => 23
+Price.create "region" => "Los Rios", "date" => Date.new(2014, 11, 14), "price" => 19
+Price.create "region" => "Los Rios", "date" => Date.new(2014, 11, 15), "price" => 21
+Price.create "region" => "Los Rios", "date" => Date.new(2014, 11, 16), "price" => 20
+Price.create "region" => "Esmeraldas", "date" => Date.new(2014, 11, 14), "price" => 24
+Price.create "region" => "Esmeraldas", "date" => Date.new(2014, 11, 15), "price" => 25
+Price.create "region" => "Esmeraldas", "date" => Date.new(2014, 11, 16), "price" => 24
+Price.create "region" => "Esmeraldas", "date" => Date.new(2014, 11, 17), "price" => 23
+
+
